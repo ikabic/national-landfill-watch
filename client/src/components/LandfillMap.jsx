68033b9d34599d58b3cd35660bfb5fe390590577
ikabic <kabic.ivona@gmail.com>
2025-10-21 17:56:20 +0200

    feat(map): add Serbia border overlay using OSM GeoJSON

--- client/src/components/LandfillMap.jsx
+++ client/src/components/LandfillMap.jsx
@@ -1,29 +1,37 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, GeoJSON } from "react-leaflet";
 
-import 'leaflet/dist/leaflet.css';
+import "../css/LandfillMap.css";
+import "leaflet/dist/leaflet.css";
 
 function LandfillMap() {
   const [landfills, setLandfills] = useState([]);
+  const [border, setBorder] = useState(null);
 
   useEffect(() => {
-    axios.get('/api/landfills')
-      .then(res => setLandfills(res.data))
-      .catch(err => console.error(err));
+    axios.get("/api/landfills")
+      .then((res) => setLandfills(res.data))
+      .catch((err) => console.error(err));
+
+    axios.get("/serbia-border.geojson")
+      .then((res) => setBorder(res.data))
+      .catch((err) => console.error("Failed to load border:", err));
   }, []);
 
-  return <MapContainer center={[44.8176, 20.4569]} zoom={8} style={{ height: '100vh', width: '100%' }}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution="&copy; OpenStreetMap contributors"
-      />
-      {landfills.map(lf => (
-        <Marker key={lf.id} position={[lf.lat, lf.lng]}>
-          <Popup>{lf.name} ({lf.category})</Popup>
-        </Marker>
-      ))}
-    </MapContainer>
+  return <MapContainer className="map" center={[44.8176, 20.4569]} zoom={8} minZoom={7} zoomSnap={0} wheelPxPerZoomLevel={100}>
+    <TileLayer url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png" />
+
+    {border && <GeoJSON data={border} style={{ color: "#d18135ff", weight: 2, fillOpacity: 0 }} />}
+
+    {landfills.map((lf) => (
+      <Marker key={lf.id} position={[lf.lat, lf.lng]}>
+        <Popup>
+          {lf.name} ({lf.category})
+        </Popup>
+      </Marker>
+    ))}
+  </MapContainer>
 }
 
 export default LandfillMap;
