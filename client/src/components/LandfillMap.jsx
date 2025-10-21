d1cead957d0e8bc471822a0224a101fab5c817fc
ikabic <kabic.ivona@gmail.com>
2025-10-21 15:31:34 +0200

    chore: switch frontend from CRA to Vite

--- client/src/components/LandfillMap.jsx
+++ client/src/components/LandfillMap.jsx
@@ -1,19 +1,19 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
 import 'leaflet/dist/leaflet.css';
 
 function LandfillMap() {
   const [landfills, setLandfills] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:5299/api/landfills')
+    axios.get('/api/landfills')
       .then(res => setLandfills(res.data))
       .catch(err => console.error(err));
   }, []);
 
-  return (
-    <MapContainer center={[44.8176, 20.4569]} zoom={8} style={{ height: '100vh', width: '100%' }}>
+  return <MapContainer center={[44.8176, 20.4569]} zoom={8} style={{ height: '100vh', width: '100%' }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution="&copy; OpenStreetMap contributors"
@@ -24,7 +24,6 @@ function LandfillMap() {
         </Marker>
       ))}
     </MapContainer>
-  );
 }
 
 export default LandfillMap;
