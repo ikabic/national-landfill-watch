d03a9ddf5c735bff33912b5d0b6b1e4965e0b698
Svetlana Kis <svetlanakis2003@gmail.com>
2025-10-21 21:16:23 +0200

    feat: add initial endpoints to landfills controller

--- server/controllers/LandfillsController.cs
+++ server/controllers/LandfillsController.cs
@@ -21,5 +21,38 @@ namespace server.controllers
             var landfills = await _context.Landfills.ToListAsync();
             return Ok(landfills);
         }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var landfill = await _context.Landfills.FindAsync(id);
+            if (landfill == null)
+                return NotFound();
+            return Ok(landfill);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] Landfill newLandfill)
+        {
+            _context.Landfills.Add(newLandfill);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetById), new { id = newLandfill.Id }, newLandfill);
+        }
+
+        [HttpPost("{id}/image")]
+        public async Task<IActionResult> UploadImage(int id, IFormFile file)
+        {
+            var landfill = await _context.Landfills.FindAsync(id);
+            if (landfill == null)
+                return NotFound();
+
+            var path = Path.Combine("wwwroot/images/landfills", $"{id}_{file.FileName}");
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            return Ok(new { path });
+        }
     }
 }
